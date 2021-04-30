a6dd4e5faf952260da07a66527a98d13b8ca6f9e
dsrgva <77065650+dsrgva@users.noreply.github.com>
2021-04-30 23:11:37 +0300

    smart contract tests using mocha&chai

---
+++ blockchain/test/test.js
@@ -0,0 +1,34 @@
+contract('Credit', (accounts) => {
+    let tempcredit;
+    var mainAccount = accounts[0];
+    const sum = 100
+    const bonus = 100
+    const period = 4
+    before( async () => {
+        tempcredit = await Credit.deployed();
+    })
+    
+    describe('when we create contract firstly', () => {
+        it('it should be 500000 bigflextokens on account', async function(){
+            const count = await tempcredit.tokenBalance(mainAccount);
+            expect(count.valueOf().toNumber()).to.be.eq(500000, "there is no 500000 bigflextokens on account");
+        })
+    });
+
+   describe('when debt is create', () =>{
+        it('should send less tokens than 500000', async function(){
+            var pass = true
+            try {
+                await tempcredit.createDebt(sum, bonus, period)
+            } catch (error) {
+                pass = false
+            }
+            assert.isTrue(pass, 'not enough tokens to create debt')
+        })
+        it('should send tokens from sender', async function(){
+            const senderBalance = (await tempcredit.tokenBalance(mainAccount)).toNumber()
+            await tempcredit.createDebt(sum, bonus, period)
+            assert.equal((await tempcredit.tokenBalance(mainAccount)).toNumber(), senderBalance - sum, 'tokens not sending')
+        })
+    })
+})
